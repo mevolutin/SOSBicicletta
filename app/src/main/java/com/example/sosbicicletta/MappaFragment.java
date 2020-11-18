21cd7a6ad0e232e3a4bfda18431045706791ed75
cristian <cristianperniconi@gmail.com>
2020-11-18 15:37:54 +0100

    serpa

--- app/src/main/java/com/example/sosbicicletta/MappaFragment.java
+++ app/src/main/java/com/example/sosbicicletta/MappaFragment.java
@@ -90,7 +90,7 @@ public class MappaFragment extends Fragment implements OnMapReadyCallback, Googl
         } else {
             ActivityCompat.requestPermissions(getActivity(), new String[]
                             {Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_LOCATION_PERMISSION);
+                    REQUEST_LOCATION_PERMISSION);//sfd
         }
     }
 
